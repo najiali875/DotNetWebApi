d61942a27e56e10c38322480f4cecd359f006065
Naji mohamed ali <najiali875@gmail.com>
2018-11-24 17:11:51 +0530

    Initail Commit

---
+++ DotNetWebApi/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using DotNetWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetWebApi.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+    }
+}
